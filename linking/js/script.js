de76ee772af8e831487ddb9f32e7c35dfaf6455b
varavoi <institut93@gmail.com>
2024-06-06 19:52:07 +0300

    Новое обновление

---
+++ linking/js/script.js
@@ -0,0 +1,9 @@
+// const script = document.createElement('script');
+// script.src = 'script2.js';          
+// // script.async = false;
+// // script.defer = true;
+// document.body.appendChild(script);
+
+
+alert("111")
+
